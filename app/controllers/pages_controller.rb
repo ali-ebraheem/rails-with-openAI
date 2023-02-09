f5fb02e32d4fb2c1a48d1742b09fa2ed565fff2d
ali-ebraheem <egm3024@uomustansiriyah.edu.iq>
2023-02-09 06:59:51 -0800

    second commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,17 @@ class PagesController < ApplicationController
 
   def api_request
     prompt = params[:prompt]
-    client = OpenAI::Client.new(access_token: "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")
+    client = OpenAI::Client.new(access_token: "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")
     client.models.list
     client.models.retrieve(id: "text-ada-001")
     response = client.completions(
       parameters: {
           model: "text-davinci-001",
           prompt: prompt,
-          max_tokens: 100
+          max_tokens: 100,
       })
       render json: JSON.parse(response.body)["choices"].map { |c| c["text"].delete("\n\n") }
   end
 
 end
+ 
